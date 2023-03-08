9493f10279c45d7e3568a4fef55fb47710ac3992
Sehwan <sehwan.lee.dev@gmail.com>
2023-03-08 16:53:23 -0400

    Finished assignment

--- routes/create.js
+++ routes/create.js
@@ -22,7 +22,10 @@ router.get('/', async function(req, res, next) {
 
   category.forEach(item => {
     if(item.name !== 'Uncategorized')
-    categories.push(item.name)
+    categories.push({
+      id:item.id,
+      name:item.name
+    })
   })
   // console.log(categories)
 
@@ -37,8 +40,49 @@ router.post('/', upload.single('featured_image'), function(req, res, next){
   console.log(req.file.buffer);
   console.log(req.body);
 
+  let associatedPostId ="";
 
-  res.redirect('/');
+  // Create post by using wpAPI
+  wp.posts().create({
+      // "title" and "content" are the only required properties
+      title: req.body.title,
+      content: req.body.content,
+      categories: req.body.category,
+      // Post will be created as a draft by default if a specific "status"
+      // is not specified
+      status: 'publish'
+  }).then(function( response ) {
+      // "response" will hold all properties of your newly-created post,
+      // including the unique `id` the post was assigned on creation
+      console.log( response.id );
+      associatedPostId = response.id;
+  }).catch(function(err){
+      //handle error
+      console.log(err);
+  })
+
+  wp.media()
+      // Specify a path to the file you want to upload, or a Buffer
+      .file(req.file.buffer, req.file.originalname)
+      .create({
+          title: `${req.body.title}_image`,
+          alt_text: `${req.body.title}_image`,
+          caption: req.body.title,
+          description: `${req.body.title}_image`
+      })
+      .then(function( response ) {
+          // Your media is now uploaded: let's associate it with a post
+          var newImageId = response.id;
+          return wp.posts().id( associatedPostId ).update({
+              featured_media: newImageId
+          });
+      })
+      .then(function( response ) {
+          console.log( 'Media ID #' + response.id );
+          console.log( 'is now associated with Post ID #' + response.post );
+      }).then(()=>{
+        res.redirect('/');
+      });
 });
 
 module.exports = router;
